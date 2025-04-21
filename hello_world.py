d1a6124fcddcf3f8a1dbec2b70e7db026f5b9cc6
Rohan beniwal <rohanbeniwal7587@gmail.com>
2025-04-21 12:13:20 +0530

    Create  hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# Hello World Python Project
+
+This is my first Python project where I write a simple "Hello, World!" script.
+
+## How to Run:
+1. Clone the repository.
+2. Run `python hello_world.py` in your terminal.
+
